5c7ec462379ed9ac50d8c7c72b57fb36d4300eab
MathewManton <mathewmanton@gmail.com>
2020-12-20 14:33:20 -0500

    Added in NFL football teams with team icons

--- app/src/main/java/com/example/sportstripplanner/MapsActivity.java
+++ app/src/main/java/com/example/sportstripplanner/MapsActivity.java
@@ -226,6 +226,39 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     {
         HashMap<String, Team> footballTeams = new HashMap<String, Team>();
 
+        footballTeams.put(getString(R.string.arizonaNFL), new Team(getString(R.string.arizonaNFL), new LatLng(33.5276247, -112.2625593),"State Farm Stadium", R.drawable.arizona_cardinals));
+        footballTeams.put(getString(R.string.atlantaNFL), new Team(getString(R.string.atlantaNFL), new LatLng(33.755463, -84.4009794),"Mercedes-Benz Stadium", R.drawable.atlanta_falcons));
+        footballTeams.put(getString(R.string.buffaloNFL), new Team(getString(R.string.buffaloNFL), new LatLng(42.7737546, -78.7869723),"Bills Stadium", R.drawable.buffalo_bills));
+        footballTeams.put(getString(R.string.baltimoreNFL), new Team(getString(R.string.baltimoreNFL), new LatLng(39.2779876, -76.6227044),"M&T Bank Stadium", R.drawable.baltimore_ravens));
+        footballTeams.put(getString(R.string.carolinaNFL), new Team(getString(R.string.carolinaNFL), new LatLng(35.2258108, -80.8528465),"Bank of America Stadium", R.drawable.carolina_panthers));
+        footballTeams.put(getString(R.string.cincinnatiNFL), new Team(getString(R.string.cincinnatiNFL), new LatLng(39.0954576, -84.5160577),"Paul Brown Stadium", R.drawable.cincinnati_bengals));
+        footballTeams.put(getString(R.string.clevelandNFL), new Team(getString(R.string.clevelandNFL), new LatLng(41.5060535, -81.6995481),"FirstEnergy Stadium", R.drawable.cleveland_browns));
+        footballTeams.put(getString(R.string.chicagoNFL), new Team(getString(R.string.chicagoNFL), new LatLng(41.8623132, -87.6166884),"Soldier Field", R.drawable.chicago_bears));
+        footballTeams.put(getString(R.string.dallasNFL), new Team(getString(R.string.dallasNFL), new LatLng(32.7472844, -97.0944939),"AT&T Stadium", R.drawable.dallas_cowboys));
+        footballTeams.put(getString(R.string.denverNFL), new Team(getString(R.string.denverNFL), new LatLng(39.7438895, -105.0201094),"Empower Field at Mile High", R.drawable.denver_broncos));
+        footballTeams.put(getString(R.string.detroitNFL), new Team(getString(R.string.detroitNFL), new LatLng(42.3400064, -83.045603),"Ford Field", R.drawable.detroit_lions));
+        footballTeams.put(getString(R.string.greenbayNFL), new Team(getString(R.string.greenbayNFL), new LatLng(44.5013406, -88.0622083),"Lambeau Field", R.drawable.greenbay_packers));
+        footballTeams.put(getString(R.string.houstonNFL), new Team(getString(R.string.houstonNFL), new LatLng(29.6847219, -95.4107074),"NRG Stadium", R.drawable.houston_texans));
+        footballTeams.put(getString(R.string.indianapolisNFL), new Team(getString(R.string.indianapolisNFL), new LatLng(39.7601007, -86.1638877),"Lucas Oil Stadium", R.drawable.indianapolis_colts));
+        footballTeams.put(getString(R.string.kansasNFL), new Team(getString(R.string.kansasNFL), new LatLng(39.0489391, -94.4839157),"Arrowhead Stadium", R.drawable.kansas_chiefs));
+        footballTeams.put(getString(R.string.losAngelesCNFL), new Team(getString(R.string.losAngelesCNFL), new LatLng(33.9533849, -118.338712),"SoFi Stadium", R.drawable.los_angeles_chargers));
+        footballTeams.put(getString(R.string.losAngelesRNFL), new Team(getString(R.string.losAngelesRNFL), new LatLng(33.9533849, -118.338712),"SoFi Stadium", R.drawable.los_angeles_rams));
+        footballTeams.put(getString(R.string.jacksonvilleNFL), new Team(getString(R.string.jacksonvilleNFL), new LatLng(30.3239536, -81.6372528),"TIAA Bank Field", R.drawable.jacksonville_jaguars));
+        footballTeams.put(getString(R.string.miamiNFL), new Team(getString(R.string.miamiNFL), new LatLng(25.9579665, -80.2388604),"Hard Rock Stadium", R.drawable.miami_dolphins));
+        footballTeams.put(getString(R.string.minnesotaNFL), new Team(getString(R.string.minnesotaNFL), new LatLng(44.9736461, -93.2574945),"U.S. Bank Stadium", R.drawable.minnesota_vikings));
+        footballTeams.put(getString(R.string.newEnglandNFL), new Team(getString(R.string.newEnglandNFL), new LatLng(42.0909458, -71.2643465),"Gillette Stadium", R.drawable.new_england_patriots));
+        footballTeams.put(getString(R.string.newOrleansNFL), new Team(getString(R.string.newOrleansNFL), new LatLng(29.951061, -90.0812442),"Mercedes-Benz Superdome", R.drawable.new_orleans_saints));
+        footballTeams.put(getString(R.string.newYorkGNFL), new Team(getString(R.string.newYorkGNFL), new LatLng(40.8135064, -74.074457),"MetLife Stadium", R.drawable.new_york_giants));
+        footballTeams.put(getString(R.string.newYorkJNFL), new Team(getString(R.string.newYorkJNFL), new LatLng(40.8135064, -74.074457),"MetLife Stadium", R.drawable.new_york_jets));
+        footballTeams.put(getString(R.string.oaklandNFL), new Team(getString(R.string.oaklandNFL), new LatLng(36.090889, -115.1833168),"Allegiant Stadium", R.drawable.oakland_raiders));
+        footballTeams.put(getString(R.string.philadelphiaNFL), new Team(getString(R.string.philadelphiaNFL), new LatLng(39.900789, -75.1674341),"Lincoln Financial Field", R.drawable.philadelphia_eagles));
+        footballTeams.put(getString(R.string.sanFranciscoNFL), new Team(getString(R.string.sanFranciscoNFL), new LatLng(37.4033851, -121.9694287),"Levi's Stadium", R.drawable.san_francisco_49ers));
+        footballTeams.put(getString(R.string.seattleNFL), new Team(getString(R.string.seattleNFL), new LatLng(47.5951518, -122.3316394),"Lumen Field", R.drawable.seattle_seahawks));
+        footballTeams.put(getString(R.string.pittsburghNFL), new Team(getString(R.string.pittsburghNFL), new LatLng(40.4467648, -80.0157603),"Heinz Field", R.drawable.pittsburgh_steelers));
+        footballTeams.put(getString(R.string.tampaNFL), new Team(getString(R.string.tampaNFL), new LatLng(27.9758691, -82.5033344),"Raymond James Stadium", R.drawable.tampa_bay_buccaneers));
+        footballTeams.put(getString(R.string.tennesseeNFL), new Team(getString(R.string.tennesseeNFL), new LatLng(36.166479, -86.7712898),"Nissan Stadium", R.drawable.tennessee_titans));
+        footballTeams.put(getString(R.string.washingtonNFL), new Team(getString(R.string.washingtonNFL), new LatLng(38.9076433, -76.8667341),"FedExField", R.drawable.washington_football_team));
+
         return footballTeams;
     }
 
